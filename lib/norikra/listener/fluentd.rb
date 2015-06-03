56217cf542d81b6e95aea4a1c54f2fd87b571955
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-06-03 20:01:54 +0900

    v0.0.1

--- lib/norikra/listener/fluentd.rb
+++ lib/norikra/listener/fluentd.rb
@@ -4,20 +4,24 @@ require "fluent-logger"
 module Norikra
   module Listener
     class Fluentd < Norikra::Listener::Base
-      def self.check(group_name)
-        group_name && group_name =~ /^FLUENTD\((.+)\)$/ && $1
+      def self.label
+        "FLUENTD"
       end
 
-      def initialize(query_name, query_group, events_statistics)
+      def initialize(argument, query_name, query_group)
         super
-        @host, port = Fluentd.check(query_group).split(':', 2)
-        raise Norikra::ArgumentError, "host is not specified" unless @host
-        @port = port ? port.to_i : 24224
-        ## TODO: initialize fluent-logger
+        addr, tag_prefix = argument.split(',', 2)
+        host, port_str = addr.split(':', 2)
+        raise Norikra::ArgumentError, "host is not specified" unless host
+        port = port_str ? port_str.to_i : 24224
+        @tag = tag_prefix ? "#{tag_prefix}.#{query_name}" : query_name
+        @log = Fluent::Logger::FluentLogger.new(nil, host: host, port: port)
       end
 
       def process_async(events)
-        ### logger
+        events.each do |time, record|
+          @log.post_with_time(@tag, record, time)
+        end
       end
     end
   end
--- norikra-listener-fluentd.gemspec
+++ norikra-listener-fluentd.gemspec
@@ -6,8 +6,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["TAGOMORI Satoshi"]
   spec.email         = ["tagomoris@gmail.com"]
   spec.summary       = %q{Norikra listener plugin to send events to Fluentd}
-  spec.homepage      = ""
-  spec.license       = "MIT"
+  spec.homepage      = "https://github.com/tagomoris/norikra-listener-fluentd"
+  spec.license       = "GPLv2"
 
   spec.files         = `git ls-files -z`.split("\x0")
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
@@ -17,6 +17,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
-  spec.add_runtime_dependency "norikra"
+  spec.add_development_dependency "rspec"
+  spec.add_runtime_dependency "norikra", ">= 1.3.0"
   spec.add_runtime_dependency "fluent-logger"
 end
